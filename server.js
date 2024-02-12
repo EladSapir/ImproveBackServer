97174ff452ff38d8ca08ddb025c73979e39d6a04
Elad Sapir <elad.1999.1999@gmail.com>
2024-02-12 18:42:39 +0200

    port try

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const dotenv = require ("dotenv").config()
 
-app.listen(443,()=>{
-    console.log('server started on port ' + 443)
+app.listen(80,()=>{
+    console.log('server started on port ' + 80)
 })
 

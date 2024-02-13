91d63138ab6f1d18abafcf90b8b428e4cedfccb1
Elad Sapir <elad.1999.1999@gmail.com>
2024-02-13 15:28:13 +0200

    port try with server

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const app = require('./app');
 const dotenv = require ("dotenv").config()
 
-app.listen(80,()=>{
-    console.log('server started on port ' + 80)
-})
+// app.listen(80,()=>{
+//     console.log('server started on port ' + 80)
+// })
+
+const server = http.createServer((req, res) => {
+    res.writeHead(403, {'Content-Type': 'text/plain'});
+    res.end('Access Denied');
+});
+
+server.listen(80, () => {
+    console.log('Server running on port 80');
+});
 

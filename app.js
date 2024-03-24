54ae651754ebe2b653e23f412ac2dfbdb9038af4
Elad Sapir <elad.1999.1999@gmail.com>
2024-03-24 14:01:26 +0200

    code fix

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express();
 const dotenv = require ("dotenv").config()
 const routes= require ('./routes/Routes')
-//const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-//mongoose.connect(process.env.DATABASE_URL);
-//const db= mongoose.connection
-//db.on("error",(err)=>console.log(err))
-//db.once("open",()=>console.log("connected to database"))
 
+mongoose.connect(process.env.DATABASE_URL)
+  .then((result) => {
+    console.log('Connected to the DataBase successfully');
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))

0cccdb3be3d1e35702b029125b61e0f690a2ddd1
Elad Sapir <elad.1999.1999@gmail.com>
2024-07-03 15:48:29 +0300

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express();
-const routes= require ('./routes/Routes')
-const bodyParser = require('body-parser')
+const routes= require ('./routes/Routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-
-
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
